63da0c828e3ace6d2b071d5eef3b8b9c9da01b19
Matt Harrah (frizbog) <frizbog1@gmail.com>
2016-06-19 17:59:41 -0400

    Checkpoint

--- src/main/java/org/g_lint/analyzer/impl/FactsWithoutSourcesAnalyzer.java
+++ src/main/java/org/g_lint/analyzer/impl/FactsWithoutSourcesAnalyzer.java
@@ -1,10 +1,15 @@
 package org.g_lint.analyzer.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.g_lint.analyzer.AResult;
 import org.g_lint.analyzer.IAnalyzer;
+import org.g_lint.analyzer.IndividualRelatedResult;
 import org.gedcom4j.model.Gedcom;
+import org.gedcom4j.model.Individual;
+import org.gedcom4j.model.IndividualEvent;
+import org.gedcom4j.model.PersonalName;
 
 /**
  * @author frizbog
@@ -17,7 +22,20 @@ public class FactsWithoutSourcesAnalyzer implements IAnalyzer {
      */
     @Override
     public List<AResult> analyze(Gedcom g) {
-        throw new UnsupportedOperationException("This analysis has not been implemented yet");
+        List<AResult> result = new ArrayList<>();
+        for (Individual i : g.individuals.values()) {
+            for (PersonalName n : i.names) {
+                if (n.citations.isEmpty()) {
+                    result.add(new IndividualRelatedResult(i, "Name", n.toString(), "No source citations"));
+                }
+            }
+            for (IndividualEvent e : i.events) {
+                if (e.citations.isEmpty()) {
+                    result.add(new IndividualRelatedResult(i, e.type.display, e.toString(), "No source citations"));
+                }
+            }
+        }
+        return result;
     }
 
     /**
